6e88d7a3fae8a5f897ce1849abeb9795a0953fba
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 00:20:01 +0800

    list of given number from instructor is created

---
+++ selection_sort.py
@@ -0,0 +1,6 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+nums = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
