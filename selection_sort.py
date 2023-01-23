57594b37741864ed070110cb02d7bfb0d893e957
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 00:46:17 +0800

    Furnishing the printing statement of the program

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,9 @@
-print("\n\t*********  PROGRAMMED BY  ********")
-print("\t***** JIMWELL L. MANGULABNAN *****")
-print("\t********** BSCOE 2-2 *************")
+print("\n\t\t\t\t*********  PROGRAMMED BY  ********")
+print("\t\t\t\t***** JIMWELL L. MANGULABNAN *****")
+print("\t\t\t\t********** BSCOE 2-2 *************")
+print()
+
+print("\t\t\t===========This is Selection Sort============")
 print()
 
 number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
@@ -16,8 +19,8 @@ def sort(number):
         number[i] = number[minimum_position]
         number[minimum_position] = swap
 
-        print(number)
+        print("\t\t\tThis is step by step process in selection sort: \n\n\t\t\t====>",number,"\n")
 
 sort(number)
 
-print(number)
+print("\n\t\t\t---> This is the Final Sorting in Selection <--- \n\n\t\t\t====>",number)
