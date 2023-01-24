bb87e71fb04e0975b3f3e12630aabce7750fca7e
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 14:19:48 +0800

    Given the array that contains random number

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+print("\n\t\t\t\t*********  PROGRAMMED BY  ********")
+print("\t\t\t\t***** JIMWELL L. MANGULABNAN *****")
+print("\t\t\t\t********** BSCOE 2-2 *************")
+print()
+
+print("\t\t\t===========This is Bubble Sort============")
+print()
+
+number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
