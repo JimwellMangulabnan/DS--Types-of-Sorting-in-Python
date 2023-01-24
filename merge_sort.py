774c0240c5f8333a7f801343124608f3842798d2
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 16:31:28 +0800

    Display the student profile and array contains unsorted number

---
+++ merge_sort.py
@@ -0,0 +1,13 @@
+print("\n\t\t\t*************  PROGRAMMED BY  ************")
+print("\t\t\t*********** JIMWELL L. MANGULABNAN *******")
+print("\t\t\t*************** BSCOE 2-2 ****************")
+print()
+
+print("\t\t\t============This is Merge Sort============")
+print()
+
+number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
+
+
+
+
