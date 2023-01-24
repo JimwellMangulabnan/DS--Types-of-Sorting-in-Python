dcf44f931752f184b876d0065df219731a0c4c8f
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 18:51:00 +0800

    Established a def quicksort for sorting array

--- quick_sort.py
+++ quick_sort.py
@@ -10,4 +10,11 @@ print()
 number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
 print("\t\t\tArray:",number)
 
+def quickSort(array, left, right):
+    if left < right:
+
+        pi = partition(array, left, right)
+        quickSort(array, left, pi - 1)
+        quickSort(array, pi + 1, right)
+
 
