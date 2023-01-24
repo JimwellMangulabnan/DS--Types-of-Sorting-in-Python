58a40432abdc09ae6e2ac5d7e3b7931572d7d83a
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-24 15:04:49 +0800

    let use def for BubbleSort that contains for statement

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,16 @@
-print("\n\t\t\t\t*********  PROGRAMMED BY  ********")
-print("\t\t\t\t***** JIMWELL L. MANGULABNAN *****")
-print("\t\t\t\t********** BSCOE 2-2 *************")
+print("\n\t\t\t***************  PROGRAMMED BY  **********")
+print("\t\t\t*********** JIMWELL L. MANGULABNAN *******")
+print("\t\t\t**************** BSCOE 2-2 ***************")
 print()
 
 print("\t\t\t===========This is Bubble Sort============")
 print()
 
 number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
+
+def BubbleSort(numbers):
+    n = len(numbers)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+           
