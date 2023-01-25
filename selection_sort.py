8e23a8c65089f4eba7e6f160391c042aee123737
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-25 23:38:13 +0800

    correcting the number of range to sort orderly

--- selection_sort.py
+++ selection_sort.py
@@ -7,9 +7,10 @@ print("\t\t\t===========This is Selection Sort============")
 print()
 
 number = [7, 62, 85, 72, 45, 77, 63, 5, 88, 30]
-
+print("\t\t\tArray: ", number)
+print()
 def sort(number):
-    for i in range(7):
+    for i in range(9):
         minimum_position = i
         for j in range(i,10):
             if number[j] < number[minimum_position]:
